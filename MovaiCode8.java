7383134dc84b4472751ac885e96dd9b084d6180e
Lommoez <etude.killbern@gmail.com>
2022-02-10 18:37:27 +0100

    déplacement des inners class dans la fonction
    permet de résoudre des conflits de type et bcp d'autres choses
    (et rendre le code plus dégueulasse au passage)

--- MovaiCode8.java
+++ MovaiCode8.java
@@ -1,94 +1,89 @@
 import java.util.*;
-import java.util.function.Consumer;
 
 public class MovaiCode8{
 
-    private class CrepeExeptionnelle extends Exception{
-        private final Object imprevu = null;
-        Object objet;
+    public static <E> E[] mange_une_crepe(E[] pile_de_crepes) throws Exception{
 
-        public CrepeExeptionnelle(){
-            super();
-            objet = imprevu;
+        class CrepeExeptionnelle extends Exception{
+            public final E imprevu = null;
+            public final E crepe;
+
+            public CrepeExeptionnelle(){
+                super();
+                crepe = imprevu;
+            }
+
+            public CrepeExeptionnelle(E crepe){
+                super();
+                this.crepe = crepe;
+            }
+
         }
 
-        public CrepeExeptionnelle(Object o){
-            super();
-            objet = o;
-        }
+        class Iterateur {
+            final E[] pile_de_crepe;
+            final int nombre_de_crepe;
+            int index = -1;
 
-        public Object getObjet(){
-            return objet;
-        }
+            public Iterateur(E[] pile_de_crepe){
+                this.pile_de_crepe = pile_de_crepe.clone();
+                this.nombre_de_crepe = pile_de_crepe.length;
+            }
 
-    }
+            public void aUnSuivant() throws CrepeExeptionnelle {
+                if(index>= nombre_de_crepe -1) throw new CrepeExeptionnelle();
+            }
 
-    private class Iterateur<E> implements Iterator<E> {
-        List<E> liste;
-        int index = -1;
-
-        public Iterateur(List<E> liste){
-            this.liste = liste;
-        }
-
-        public void aUnSuivant() throws CrepeExeptionnelle {
-            if(index>=liste.size()-1) throw new CrepeExeptionnelle();
-        }
-
-        public void suivant() throws  CrepeExeptionnelle {
-            aUnSuivant();
-            throw new CrepeExeptionnelle(liste.get(++index));
-        }
-
-        @Override
-        public boolean hasNext() {
-            try{
+            public void suivant() throws  CrepeExeptionnelle {
                 aUnSuivant();
-            }catch (CrepeExeptionnelle crepeExeptionnelle){
-                return false;
+                throw new CrepeExeptionnelle(pile_de_crepe[++index]);
             }
-            return true;
-        }
 
-        @Override
-        public E next() {
-            E objet = null;
-            try {
-                suivant();
-            }catch (CrepeExeptionnelle crepeExeptionnelle){
-                if(crepeExeptionnelle.objet==null) throw new NoSuchElementException();
-                objet = (E) crepeExeptionnelle.getObjet();
+            public boolean hasNext() {
+                try{
+                    aUnSuivant();
+                }catch (CrepeExeptionnelle crepeExeptionnelle){
+                    return false;
+                }
+                return true;
             }
-            return objet;
+
+            public E nnext() {
+                try {suivant();}
+                catch (CrepeExeptionnelle crepeExeptionnelle) {
+                    if (crepeExeptionnelle.crepe != null)
+                    return crepeExeptionnelle.crepe;
+                }
+                throw new NoSuchElementException();
+            }
         }
 
-        @Override
-        public void remove() {
-            liste.remove(index);
-        }
 
-        @Override
-        public void forEachRemaining(Consumer action) {
-            Iterator.super.forEachRemaining(action);
-        }
-    }
 
-    public List<Object> mange_une_crepe(List<Object> pile_de_crepes){
-        Stack<Object> stack = new Stack<>();
-        Iterateur<Object> iterateur = new Iterateur<Object>(pile_de_crepes);
-        while(iterateur.hasNext()){
-            stack.add(iterateur.next());
+        Stack<E> stack = new Stack<>();
+        Iterateur iterateur = new Iterateur(pile_de_crepes);
+
+        try{
+            while (true){
+                iterateur.aUnSuivant();
+                try{ iterateur.suivant(); }
+                catch( CrepeExeptionnelle crepeExeptionnelle){
+                    if(crepeExeptionnelle.crepe==null)
+                        throw new NoSuchElementException();
+                    stack.add(crepeExeptionnelle.crepe);
+                }
+            }
+        }catch (CrepeExeptionnelle crepeExeptionnelle){
+
         }
         stack.pop();
-        pile_de_crepes = stack.subList(0, stack.size());
+
+        pile_de_crepes = stack.toArray(pile_de_crepes);
         return pile_de_crepes;
     }
 
-    public static void main(String[] args) {
-        MovaiCode8 movaiCode8 = new MovaiCode8();
-        ArrayList<Integer> array = new ArrayList<>();
-        array.add(0);array.add(1);array.add(2);array.add(3);array.add(4);array.add(5);
-        List<Object> li = movaiCode8.mange_une_crepe(Arrays.asList(array.toArray()));
-        Object[] ob = li.toArray();
+    public static void main(String[] args) throws Exception {
+        Integer[] array = new Integer[]{0,1,2,3};
+        Integer[] array2 = MovaiCode8.<Integer>mange_une_crepe(array);
     }
 }
